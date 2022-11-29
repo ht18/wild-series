459f9e9a50c771d8bdf80d7d9aa7647cd4be0e06
Herschel TUIL <herschel.tuil@gmail.com>
2022-11-29 11:18:32 +0100

    first commit

---
+++ src/DataFixtures/ProgramFixtures.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+use App\Entity\Program;
+
+
+class ProgramFixtures extends Fixture implements DependentFixtureInterface
+{
+    public function load(ObjectManager $manager): void
+    {
+        $program = new Program();
+        $program->setTitle('Walking dead');
+        $program->setSynopsis('Des zombies envahissent la terre');
+        $program->setCategory($this->getReference('category_Action'));
+        $manager->persist($program);
+        $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        // Tu retournes ici toutes les classes de fixtures dont ProgramFixtures dépend
+        return [
+            CategoryFixtures::class,
+        ];
+    }
+}
