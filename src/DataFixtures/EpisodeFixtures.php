d2a75de303ead398068be56b04bd04fe7048fe66
Herschel TUIL <herschel.tuil@gmail.com>
2023-01-19 11:53:36 +0100

    quest12

---
+++ src/DataFixtures/EpisodeFixtures.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Episode;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+
+//Tout d'abord nous ajoutons la classe Factory de FakerPhp
+use Faker\Factory;
+
+class EpisodeFixtures extends Fixture implements DependentFixtureInterface
+{
+    public function load(ObjectManager $manager): void
+    {
+        //Puis ici nous demandons à la Factory de nous fournir un Faker
+        $faker = Factory::create();
+
+        /**
+        * L'objet $faker que tu récupère est l'outil qui va te permettre 
+        * de te générer toutes les données que tu souhaites
+        */
+
+        for($i = 0; $i < 10; $i++) {
+            $episode = new Episode();
+            $episode->setNumber($faker->numberBetween(1, 5));
+            $episode->setSynopsis($faker->paragraphs(3, true));
+            $episode->setTitle($faker->sentence(3));
+            $episode->setSeasonId($this->getReference('season_' . $faker->numberBetween(1, 5)));
+            
+            $manager->persist($episode);
+        }
+
+        $manager->flush();
+    }
+
+    public function getDependencies(): array
+    {
+        return [
+           SeasonFixtures::class,
+        ];
+    }
+
+}
--- src/DataFixtures/ProgramFixtures.php
+++ src/DataFixtures/ProgramFixtures.php
@@ -21,33 +21,13 @@ class ProgramFixtures extends Fixture implements DependentFixtureInterface
         $manager->persist($program);
 
         $program1 = new Program();
-        $program1->setTitle('Walking dead');
+        $program1->setTitle('Walking dead 2');
         $program1->setSynopsis('Des zombies envahissent la terre');
         $program1->setCategory($this->getReference('category_Aventure'));
         $manager->persist($program1);
 
-        $season = new Season();
-        $season->setNumber(1);
-        $season->setYear(1990);
-        $season->setDescription("description season 1");
-        $season->setProgrammId($program);
-
-        $manager->persist($season);
-
-        $episode = new Episode();
-        $episode->setTitle('Title Episode 1');
-        $episode->setNumber(1);
-        $episode->setSynopsis("Synopsis Episode 1");
-        $episode->setSeasonId($season);
-
-        $episode2 = new Episode();
-        $episode2->setTitle('Title Episode 2');
-        $episode2->setNumber(2);
-        $episode2->setSynopsis("Synopsis Episode 2");
-        $episode2->setSeasonId($season);
-
-        $manager->persist($episode);
-        $manager->persist($episode2);
+        $this->addReference('program_' . 1, $program);
+        $this->addReference('program_' . 2, $program1);
 
         $manager->flush();
     }
